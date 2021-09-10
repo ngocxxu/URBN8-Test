b0c5dae1013354418bc3d1448f5281f0d01c6ca8
ngocxxu <ngocquach43@gmail.com>
2021-09-10 15:37:28 +0700

    change

---
+++ exercise-01/exercise-01.js
@@ -0,0 +1,16 @@
+let arrNumber = [5, 9, 6, 8, 4, 6];
+let sum = 0;
+
+// function sumArr(arrNum) {
+//   for (let i = 0; ; i++) {
+//     sum += arrNum[i];
+//     console.log(sum);
+//     if (sum > 38) {
+//       sumArr(arrNum);
+//     }
+//   }
+// }
+
+console.log("hello");
+
+sumArr(arrNumber);
